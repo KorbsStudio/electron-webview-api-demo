dfb4d792f9cf7475288900b84b2cf9abf248bdbc
Korbs <51213244+KorbsStudio@users.noreply.github.com>
2022-05-09 21:53:26 -0400

    Keep the basic template

--- main.js
+++ main.js
@@ -1,32 +1,10 @@
 const {app, BrowserWindow} = require('electron')
 
-if (process.platform == 'darwin') { // NOT REQUIRED! Feel free to remove.
-  app.whenReady().then(() => {
-    global.frame = false;
-    global.titleBarStyle = 'hiddenInset';
-  })}
-else if(process.platform == 'win32'){
-  app.whenReady().then(() => {
-    global.frame = false;
-    global.titleBarStyle = 'hidden';
-  })}
-else{
-  app.whenReady().then(() => {
-    global.frame = true;
-    global.titleBarStyle = 'hidden';
-  })}
-
 function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 1500,
     height: 900,
     autoHideMenuBar: true,
-    frame: global.frame, // NOT REQUIRED! Feel free to remove.
-    titleBarStyle: global.titleBarStyle, // NOT REQUIRED! Feel free to remove.
-    titleBarOverlay: {
-      color: '#191919',
-      symbolColor: 'white'
-    },
     webPreferences: {
       webviewTag: true // Required for any webview to be loaded
     }
